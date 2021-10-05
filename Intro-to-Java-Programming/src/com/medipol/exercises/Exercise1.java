1c9817089e8cb75efb2f3c1537739ea0f381d67b
Ömer Olcay <omer_olcay25@hotmail.com>
2021-10-05 23:29:36 +0300

    Update exercise1

--- Intro-to-Java-Programming/src/com/medipol/exercises/Exercise1.java
+++ Intro-to-Java-Programming/src/com/medipol/exercises/Exercise1.java
@@ -4,11 +4,10 @@ public class Exercise1 {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Welcome to Java.");
-		System.out.println("Welcome to Java.");
-		System.out.println("Welcome to Java.");
-		System.out.println("Welcome to Java.");
-		System.out.println("Welcome to Java.");
+		System.out.println("Welcome to Java");
+		System.out.println("Welcome to Computer Science");
+		System.out.println("Programming is fun");
+	
 	}
 
 }
